79eec0abcfabe4b816c557011f0bd7ff99f6b0b2
Dmitry Egupov <egupovds@gmail.com>
2023-10-19 12:27:19 +0600

    2

---
+++ app.js
@@ -0,0 +1,29 @@
+// Получаем ссылки на элементы DOM
+const minusButton = document.querySelector(".minus-button");
+const plusButton = document.querySelector(".plus-button");
+const countElement = document.querySelector(".count");
+
+// Устанавливаем начальное значение
+let count = 0;
+
+// Функция для обновления отображаемого значения и счетчика
+function updateCount() {
+    countElement.textContent = count;
+}
+
+// Обработчик нажатия кнопки "минус"
+minusButton.addEventListener("click", () => {
+    if (count > 0) {
+        count--;
+        updateCount();
+    }
+});
+
+// Обработчик нажатия кнопки "плюс"
+plusButton.addEventListener("click", () => {
+    count++;
+    updateCount();
+});
+
+// Инициализация счетчика
+updateCount();
